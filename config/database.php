e179d9c9112fc2070e0e18a853cb018ba7aa11cf
Juraj <jmarkesic@tvz.hr>
2018-02-15 21:38:55 +0100

    Heroku db setup.

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_e039eb3996bc204'),
+            'username' => env('DB_USERNAME', 'bb9da02fdfa7b6'),
+            'password' => env('DB_PASSWORD', '59c48571'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
